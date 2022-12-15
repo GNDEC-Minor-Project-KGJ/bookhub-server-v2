b26f01f5b38e0bd92b10edaa2bd9471622ab2e24
Konark Lohat <konarklohat123456@gmail.com>
2022-12-16 00:26:34 +0530

    logs

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,6 +6,7 @@ const auth = {
     try {
       const token = req.headers.authorization.split(" ")[1];
       const decodedToken = await verifyAuthToken(token);
+      console.log(token);
 
       if (decodedToken) {
         req.userId = decodedToken.id;
